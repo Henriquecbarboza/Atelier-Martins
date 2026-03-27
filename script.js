0f370e1af06fd239d4a6ac7aff67bb48fcf5424c
Henriquecbarboza <riquecostab@gmail.com>
2026-03-26 21:27:37 -0300

    dialog

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector("custom-button");
+const button = document.querySelector("button");
 const modal = document.querySelector("dialog");
 const buttonClose = document.querySelector("dialog button");
 
