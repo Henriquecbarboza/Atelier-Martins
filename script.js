c9dee2246f5ad90dbbfbb05a4d9f9ec317447874
Henriquecbarboza <riquecostab@gmail.com>
2026-03-27 18:17:51 -0300

    projeto final

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const button = document.querySelector("button");
-const modal = document.querySelector("dialog");
-const buttonClose = document.querySelector("dialog button");
+const modal = document.getElementById("modal");
+const buttons = document.querySelectorAll(".custom-button");
+const nome = document.getElementById("produto-nome");
+const whatsapp = document.getElementById("whatsapp-link");
+const fechar = document.getElementById("fechar");
 
-button.onclick = function () {
-  modal.showModal();
-};
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const produto = btn.dataset.nome;
 
-buttonClose.onclick = function () {
-  modal.close();
-};
+    nome.textContent = produto;
+
+    whatsapp.href = `https://wa.me/5551999999999?text=Olá, quero comprar a ${produto}`;
+
+    modal.showModal();
+  });
+});
+
+fechar.onclick = () => modal.close();
